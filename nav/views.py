e1ffe3a9b3f98cb7c8adee92c3f35bb664efdf36
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-06-21 17:19:04 +0300

    For Loop

--- nav/views.py
+++ nav/views.py
@@ -7,7 +7,7 @@ def index(response, id):
     # query certain or perticular object with id=1
     item = ls.item_set.get(id=1)
 
-    return render(response, "main/base.html", {"name":ls.name})
+    return render(response, "main/list.html", {"ls":ls})
  
 def home(response):
     return render(response, "main/home.html")
