46574867455ce2b6a5dc3c54c3956b0b9a83108f
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-06-21 15:40:51 +0300

    Query Db

--- nav/views.py
+++ nav/views.py
@@ -4,4 +4,6 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" %ls.name)
+    item = ls.item_set.get(id=1)
+
+    return HttpResponse("<h1>%s</h1><br /><br /><p>%s</p>" %(ls.name, str(item.text)))
