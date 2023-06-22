7592126b9eeeb7ef202fab5d77af8f0825932e7b
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-06-22 10:01:21 +0300

    (Method[GET=> Retrival $ POST=>Send Data To the Server], Csrf tokens[Security], format the form(as_p=> in New line or in block, as_ul=> be in list, as_table))

--- nav/views.py
+++ nav/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
     # query certain or perticular object with id=1
-    item = ls.item_set.get(id=1)
+    # item = ls.item_set.get(id=1)
 
     return render(response, "main/list.html", {"ls":ls})
  
@@ -16,5 +16,15 @@ def home(response):
 
 def create(response):
     # instantiate
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        # validate
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        # to redirect to new ToDoList
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
