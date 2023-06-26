25748f458fd789067249d286d9cad4b802d5eb75
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-06-26 10:27:41 +0300

    User Authentication $ Login

--- app/settings.py
+++ app/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 # CRISPY_ALLOW_TEMPLATE_PACK ="bootstrap4"
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
+LOGIN_REDIRECT_URL = "/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
